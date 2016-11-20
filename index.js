a05235f14c15631c955b667ad6b0eabb94e34070
3846masa <3846masahiro+git@gmail.com>
2016-11-20 12:22:51 +0900

    v-for

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var app = new Vue({
   data: {
     title: 'Todo Title',
+    taskList: [
+      {
+        text: 'Todo Task 01',
+        isDone: false,
+      },
+      {
+        text: 'Todo Task 02',
+        isDone: false,
+      },
+    ],
   },
 });
 
